47225dc756ef869932a25094533a4cd97301d695
LuanKuhlmann <l.campos.kuhlmann@outlook.com>
2023-07-14 12:18:27 -0300

    Endpoint implemented UserResource, UserService

--- src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.dicasdeumdev.api.resources;
 
 import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
+    @Autowired
+    private UserService service;
 
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(new User(1, "Luan", "luan@mail.com", "12345"));
+        return ResponseEntity.ok().body(service.findById(id));
 
     }
 }
